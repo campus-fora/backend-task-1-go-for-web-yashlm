b1aaf2fa1ecdd8ef96bed0eea4344f026579a819
Yash Chauhan <yashc22@iitk.ac.in>
2023-05-05 23:10:50 +0530

    update section added

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,18 +9,32 @@ export default class extends React.Component {
         <title>Backend Task</title>
         <h1>Backend Task</h1>
         <div className='form'>
-          <div><input placeholder='enter id'></input>
+          <div>
+            <h2>Get All</h2>
+            <button>Get All!</button></div>
+          <div>
+          <h2>Post</h2>
+            <input placeholder='enter id'></input>
             <br></br>
             <input placeholder='enter text'></input>
             <br></br>
             <button>Post!</button>
           </div>
-          <div><button>Get all!</button></div>
           <div>
+          <h2>Update</h2>
+            <input placeholder='enter id'></input>
+            <br></br>
+            <input placeholder='enter text'></input>
+            <br></br>
+            <button>Update!</button>
+          </div>
+          <div>
+          <h2>Delete</h2>
+          <br></br>
             <input placeholder='enter id'></input>
             <br></br>
             <button>Delete!</button>
-          </div>
+            </div>
           </div>
 		</div>
 		)
